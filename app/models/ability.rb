5219c0c91cd743380ad6d517b2a3d25841c854d7
MosDevx <mosesk.wamae@gmail.com>
2023-05-26 16:18:17 +0300

    can can abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,20 +2,22 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
+    user ||=  User.new # guest user (not logged in)
 
     if user.role == 'admin'
       can :manage, :all
 
     elsif user.role == 'user'
-      can :read, :all
+      can :read, :all, user_id: user.id
+      can :read, Inventory , user_id: user.id
+      can :show, Inventory , user_id: user.id
       can :create, :all
-      can :update, :all
-      can :destroy, :all
-      can :show_inventory_foods, Inventory
-      can :add_food_item, Inventory
+      can :update, :all, user_id: user.id
+      can :destroy,:all, user_id: user.id
+      can :show_inventory_foods, Inventory , user_id: user.id
+      can :add_food_item, Inventory , user_id: user.id
     else
-      can :read, :all
+      # can :read, :all
     end
   end
 end
