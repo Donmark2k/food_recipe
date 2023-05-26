2d1407de70f1e5482aa7bf0d63c4a3c53b33d577
MosDevx <mosesk.wamae@gmail.com>
2023-05-26 16:23:16 +0300

    fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,22 +2,20 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||=  User.new # guest user (not logged in)
+    user ||= User.new # guest user (not logged in)
 
     if user.role == 'admin'
       can :manage, :all
 
     elsif user.role == 'user'
       can :read, :all, user_id: user.id
-      can :read, Inventory , user_id: user.id
-      can :show, Inventory , user_id: user.id
+      can :read, Inventory, user_id: user.id
+      can :show, Inventory, user_id: user.id
       can :create, :all
       can :update, :all, user_id: user.id
-      can :destroy,:all, user_id: user.id
-      can :show_inventory_foods, Inventory , user_id: user.id
-      can :add_food_item, Inventory , user_id: user.id
-    else
-      # can :read, :all
+      can :destroy, :all, user_id: user.id
+      can :show_inventory_foods, Inventory, user_id: user.id
+      can :add_food_item, Inventory, user_id: user.id
     end
   end
 end
