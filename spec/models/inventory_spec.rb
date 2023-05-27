c3dc737230f9fd17a41bea3879df511db02f70be
Donmark2k <chukwuemekavp@gmail.com>
2023-05-27 14:47:53 +0100

    Inventory test is now passing

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Inventory.new(
+      name: 'Beans',
+      user_id: User.first
+    )
+  end
+  before { subject.save }
+  describe 'check Inventort invalidity' do
+    it 'is invalid with no user_id' do
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'check inventory invalidity' do
+    it 'is invalid with no inventory name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
