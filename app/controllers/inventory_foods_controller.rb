e85debe28ff0327a9b80a67a82b60dca93484c42
MosDevx <mosesk.wamae@gmail.com>
2023-05-27 19:43:39 +0300

    fix linters

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -54,7 +54,7 @@ class InventoryFoodsController < ApplicationController
     @inventory_food.destroy
 
     respond_to do |format|
-      format.html { redirect_to request.url, notice: 'Inventory food was successfully destroyed.' }
+      format.html { redirect_to request.referrer, notice: 'Inventory food was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
