c896e00c6e63c897810e8352d50a484c2cb652c3
Donmark2k <chukwuemekavp@gmail.com>
2023-05-27 13:29:52 +0100

    Food recipe test is passing

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
 
   it 'is valid with valid attributes' do
     expect(Food.new(name: 'Anything', measurement_unit: 100, price: 200)).to be_valid
@@ -15,8 +14,8 @@ RSpec.describe Food, type: :model do
     food = Food.new(measurement_unit: nil)
     expect(food).to_not be_valid
   end
-  it 'is not valid with a name longer than 50 characters' do
-    food = Food.new(name: 'a' * 51)
+  it 'is not valid without a price' do
+    food = Food.new(price: nil)
     expect(food).to_not be_valid
   end
 end
